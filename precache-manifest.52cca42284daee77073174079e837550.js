3f2e2e0d937e683b75e9b9a237e0a2ec4510e3a5
ADITYA SHARMA <sharmaaditya13064@gmail.com>
2022-10-16 15:36:58 +0530

    Updates

---
+++ precache-manifest.52cca42284daee77073174079e837550.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47cb43580bb0ac75b842c213947102d0",
+    "url": "/light-messenger/index.html"
+  },
+  {
+    "revision": "692972a8d9d07606a91f",
+    "url": "/light-messenger/static/css/main.37448acd.chunk.css"
+  },
+  {
+    "revision": "681a4f7130795b9263f0",
+    "url": "/light-messenger/static/js/2.c124d3fa.chunk.js"
+  },
+  {
+    "revision": "cf4263b26236207b597a32c399243e67",
+    "url": "/light-messenger/static/js/2.c124d3fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "692972a8d9d07606a91f",
+    "url": "/light-messenger/static/js/main.b9f66dfd.chunk.js"
+  },
+  {
+    "revision": "cd4cee92734f5a375202",
+    "url": "/light-messenger/static/js/runtime-main.ce56f8d3.js"
+  }
+]);
